3ad6fa903ce5dce0ff996efaed97e9c725562667
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-10-15 21:33:27 +0900

    334. Increasing Triplet Subsequence

---
+++ LeetCode/334_IncreasingTripletSubsequence.swift
@@ -0,0 +1,42 @@
+class Solution {
+    func increasingTriplet(_ nums: [Int]) -> Bool {
+        
+        var first = Int.max
+        var second = Int.max
+        for i in nums {
+            if i < first {
+                first = i
+            } else if i < second {
+                second = i
+            } else if i > second {
+                return true
+            }
+        }
+        
+        
+        return false
+    }
+}
+
+
+class Test: XCTestCase {
+    func test1Code() {
+        let sol = Solution()
+        let str = sol.increasingTriplet([1, 2, 3, 4, 5])
+        XCTAssertEqual(str, true)
+    }
+    
+    func test2Code() {
+        let sol = Solution()
+        let str = sol.increasingTriplet([5, 4, 3, 2, 1])
+        XCTAssertEqual(str, false)
+    }
+    
+    func test3Code() {
+        let sol = Solution()
+        let str = sol.increasingTriplet([2, 1, 5, 0, 4, 6])
+        XCTAssertEqual(str, true)
+    }
+}
+
+Test.defaultTestSuite.run()
