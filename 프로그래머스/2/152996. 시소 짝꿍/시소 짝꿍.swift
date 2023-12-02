d412c5d3081e5f1aa70f5793ece7bc743c6810bf
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-12-02 21:13:49 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.14 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.swift
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.swift
@@ -7,19 +7,13 @@ func solution(_ weights:[Int]) -> Int64 {
     var map: [Double: Int] = [:] // 몸무게 : 인원
     
     for weight in weights {
-        let w = Double(weight)
-        if let count = map[w] {
-            map[w] = count+1
-        } else {
-            map[w] = 1
-        }
+        map[Double(weight), default: 0] += 1
     }
     
     let keys = map.keys.sorted()
     for key in keys {
         let count = map[key]!
         if count > 1 {
-            print(count, count * (count - 1) / 2)
             result += (count * (count - 1) / 2)
         }
         
