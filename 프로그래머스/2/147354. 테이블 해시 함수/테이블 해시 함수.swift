d8ff02c9dd13941926de2fd54536757dbaf6269b
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-12-26 13:33:08 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.18 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.swift
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.swift
@@ -2,23 +2,13 @@ import Foundation
 
 func solution(_ data:[[Int]], _ col:Int, _ row_begin:Int, _ row_end:Int) -> Int {
     
-    let sortData = data.sorted() {
-        if $0[col-1] == $1[col-1] {
-            return $0[0] > $1[0]
-        } 
-        return $0[col-1] < $1[col-1]
-    }
+    let sortData = data.sorted{$0[0] > $1[0]}.sorted { $0[col-1] < $1[col-1] }
     var modSumList: [Int] = []
     for i in row_begin-1..<row_end {
         modSumList.append(sortData[i].reduce(0) { 
-            return $0 + $1%(i+1)
+            $0 + $1%(i+1)
         })
     }
-    
-    var result = modSumList[0]
-    for i in 1..<modSumList.count {
-        result = result ^ modSumList[i]
-    }
-    
-    return result
+   
+    return modSumList.reduce(0, ^)
 }
