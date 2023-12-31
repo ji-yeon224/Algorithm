5e6886adf87e8168db473ceb89b02914c5d5f2a6
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-12-31 16:21:18 +0900

    Update 회의실 배정.swift

--- 백준/Silver/1931. 회의실 배정/회의실 배정.swift
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.swift
@@ -11,14 +11,16 @@ func solution() -> Int {
         let time = readLine()!.components(separatedBy: " ").map { Int($0)! }
         meetings.append((time[0], time[1]))
     }
-    
+
+    // 종료 시간이 빠른 순
     meetings.sort {
-        if $0.1 == $1.1 {
+        if $0.1 == $1.1 { // 종료 시간이 같다면 시작 시간이 빠른 순
             return $0.0 < $1.0
         }
         return $0.1 < $1.1
     }
-    
+
+    // 첫 번째 회의실이 가장 많이 들어가는 회의실
     var last = -1
     var result = 0
     for m in meetings {
