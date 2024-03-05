79b4dcace13221ad7a8add7b851e0e3b5a9a52d9
ji-yeon224 <shjh0107@gmail.com>
2024-03-05 11:24:30 +0900

    [Gold IV] Title: 타임머신, Time: 396 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -1,25 +1,34 @@
+
 import sys
-input=sys.stdin.readline
-MAX=1e9
-n, m = map(int, input().split())
-graph=[[] for _ in range(n+1)] #그래프
-arr=[MAX]*(n+1)
-arr[1]=0
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c)) #a에서 b로 가는 시간은 c
-for i in range(n): 
-    for idx, temp in enumerate(graph): #모든 간선 조사
-        if idx==0:
-            continue
-        for to_node, cost in temp:
-            if arr[idx]!=MAX and arr[idx]+cost<arr[to_node]: #현재 까지 to_node까지의 비용보다 지금 노드를 거쳐 가는 편이 저렴할 때
-                arr[to_node]=arr[idx]+cost #최단 거리 갱신
-                if i==n-1: #사이클이 존재
-                    print(-1)
-                    exit()
-for i in range(2, n+1):
-    if arr[i]==MAX:
+
+input = sys.stdin.readline
+INF = 1e9
+
+N, M = map(int, input().split())
+dict = {i: [] for i in range(1, N+1)}
+
+for i in range(M):
+    A, B, C = map(int, input().split())
+    dict[A].append((B, C))
+
+dist = [INF] * (N+1)
+
+def bellman():
+    dist[1] = 0
+    
+    for t in range(N):
+        for i in range(1, N+1):
+            for n, d in dict[i]:
+                if dist[i] != INF and dist[n] > dist[i]+d:
+                    dist[n] = dist[i]+d
+                    if t == N-1:
+                        print(-1)
+                        exit()
+                
+
+bellman()
+for i in range(2, N+1):
+    if dist[i] == INF:
         print(-1)
     else:
-        print(arr[i])
+        print(dist[i])
