6b94ca1a4d84ee75c6dfd974224d51f0a5d3c28c
ji-yeon224 <shjh0107@gmail.com>
2024-04-25 09:49:10 +0900

    [Silver I] Title: 상어 키우기, Time: 160 ms, Memory: 85432 KB -BaekjoonHub

--- 백준/Silver/30892. 상어 키우기/상어 키우기.swift
+++ 백준/Silver/30892. 상어 키우기/상어 키우기.swift
@@ -1,23 +1,4 @@
-struct Stack<T> {
-    var stack: [T] = []
-  
-    var count : Int {
-        return stack.count
-    }
-    var isEmpty : Bool {
-        return stack.isEmpty
-    }
-    
-    mutating func pop() -> T? {
-        return stack.popLast()
-    }
-    mutating func push(_ element: T) {
-        stack.append(element)
-    }
-    func top() -> T? {
-        return stack.last
-    }
-}
+
 
 let firstLine = readLine()!.split(separator: " ").map{ Int(String($0))! }
 let N = firstLine[0]
@@ -25,13 +6,13 @@ let K = firstLine[1]
 var T = firstLine[2]
 let secondLine = readLine()!.split(separator: " ").map{ Int(String($0))! }
 var sharks = secondLine.sorted(by:>)
-var stack = Stack<Int>()
+var stack: [Int] = []
 
 for _ in 0..<K {
     while !sharks.isEmpty {
 
         if sharks.last! < T {
-            stack.push(sharks.popLast()!)
+            stack.append(sharks.popLast()!)
         }
         else { break }
         
@@ -39,7 +20,7 @@ for _ in 0..<K {
     if stack.isEmpty {
         break
     } else {
-        T += stack.pop()!
+        T += stack.popLast()!
         
     }
     
