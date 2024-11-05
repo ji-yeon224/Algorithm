335014f88fafecc16ad2f00b133237c0cf792753
ji-yeon224 <shjh0107@gmail.com>
2024-11-05 12:35:29 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 60 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -1,3 +1,4 @@
+
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -6,32 +7,18 @@ N = int(input())
 stack = deque()
 buildings = 0
 for _ in range(N):
-    a, b = map(int, input().split(' '))
-    if not stack:
-        if b != 0:
-            stack.append((a, b))
-    else:
-        if stack[-1][1] > b:
-            last = stack[-1][1]
+    _, height = map(int, input().split())
+    while stack and stack[-1] > height:
+        lastH = stack.pop()
+        buildings+=1
+        while stack and stack[-1] == lastH:
             stack.pop()
-            buildings += 1
-            while stack and stack[-1][1] > b:
-                if last != stack[-1][1]:
-                    buildings += 1
-                    last = stack[-1][1]
-                stack.pop()
-            if b != 0:
-                stack.append((a, b))
-        else:
-            stack.append((a, b))
-if stack:
-    lastH = stack[-1][1]
-    stack.pop()
+    if height != 0:
+        stack.append(height)
+while stack:
+    lastH = stack.pop()
     buildings += 1
-    while stack:
-        if lastH > stack[-1][1]:
-            lastH = stack[-1][1]
-            buildings += 1
+    while stack and stack[-1] == lastH:
         stack.pop()
-
+    
 print(buildings)
